813ee76f958e59d611bb98b11bbfc85c783482c5
Pranav Patil <pranav07@somaiya.edu>
2023-10-23 12:50:31 +0530

    updated paths 8

--- server/database/db.php
+++ server/database/db.php
@@ -1,7 +1,7 @@
 <?php
 
-    require 'vendor/autoload.php';
-    require_once 'config.php';
+    require __DIR__ . '/../vendor/autoload.php';
+    require_once __DIR__ . '/../config.php';
     $user = DB_USER;
     $pass = DB_PASS;
     function getDatabase() {
