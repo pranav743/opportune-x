6cb7a0e14ecd7a2588abf5449283717f5103dde5
Pranav Patil <pranav07@somaiya.edu>
2023-10-23 12:32:32 +0530

    updated paths

--- server/index.php
+++ server/index.php
@@ -18,11 +18,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit();
 }
 
-require 'vendor/autoload.php'; // Autoload FastRoute and other dependencies
-require 'controllers/UserController.php';
-require 'controllers/InternshipController.php';
-require 'controllers/CourseController.php';
-require 'routes/user.php';
+require './vendor/autoload.php';
+require './controllers/UserController.php';
+require './controllers/InternshipController.php';
+require './controllers/CourseController.php';
+require './routes/user.php';
 require './database/db.php';
 
 $httpMethod = $_SERVER['REQUEST_METHOD'];
